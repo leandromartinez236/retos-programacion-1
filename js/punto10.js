67f570d28b0f33fe6a381fe473e7cd455c468e7f
leandro <martinezleadro2004@gmail.com>
2021-12-20 18:50:26 -0300

    ejercicios terminados

--- js/punto10.js
+++ js/punto10.js
@@ -1,7 +1,3 @@
-// Instrucciones: hay 1.609344 km en una milla (mi). Escribe un programa en el que 
-// el usuario indique una cantidad de millas y muestre en pantalla el resultado convertido a kilómetros.
-// 1 milla = 1.609344 km
-
 const milla = parseFloat(prompt('Cantidad de millas',''));
 const km = 1.609344;
 const kilometros = milla * km
--- js/punto8.js
+++ js/punto8.js
@@ -1,8 +1,3 @@
-// Instrucciones: vas con tus amigos a tu restaurante favorito y acuerdan dividir la cuenta.
-// Para facilitar las cosa pedirás al usuario que indique el total a pagar, entre cuantas personas se dvidirá la cuenta,
-//  porcentaje de propina a incluir, un porcentaje de impuestos,
-//  total a pagar incluyendo propina más impuestos y el total a pagar por cada persona.
-
 const pago = parseFloat (prompt('Ingrese el monto a pagar', ''));
 const personas = parseFloat (prompt('Cuantas personas pagarán la cuenta', ''));
 const propina = parseFloat (prompt('Propina', ''));
